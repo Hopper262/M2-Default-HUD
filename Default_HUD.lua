4a815d1b1ede2f211fc212ac7528cb62a581bb70
Hopper262 <hopper@whpress.com>
2024-06-02 20:02:17 -0400

    version 1.2

--- Default_HUD.lua
+++ Default_HUD.lua
@@ -506,6 +506,11 @@ function sfloor(num)
   return num - (num % hud_rect.scale)
 end
 
+function format_time(ticks)
+  local secs = math.floor(ticks / 30)
+  return string.format("%d:%02d", math.floor(secs / 60), secs % 60)
+end
+
 function player_ranking_text(gametype, ranking)
   if     gametype == "kill monsters" or
          gametype == "capture the flag" or
@@ -522,7 +527,7 @@ function player_ranking_text(gametype, ranking)
          gametype == "kill the man with the ball" or
          gametype == "defense" or
          gametype == "tag" then
-    return string.format("%d:%02d", math.floor(ranking/60), ranking % 60)
+    return format_time(math.abs(ranking))
   end
   return nil
 end
@@ -609,10 +614,7 @@ function network_stats_draw()
   
   -- time/score display in header
   if Game.time_remaining and Game.time_remaining <= 30*60*999 then
-    local seconds = math.floor(Game.time_remaining / 30)
-    local minutes = math.floor(seconds / 60)
-    
-    draw_text_right(f, string.format("%d:%02d", minutes, seconds % 60),
+    draw_text_right(f, format_time(Game.time_remaining),
                     r.xr, r.y, InterfaceColors["inventory text"])
     
   elseif Game.kill_limit then
@@ -625,7 +627,7 @@ function network_stats_draw()
        gt == "tag" then
       local leastleft = nil
       for p = 1,#Game.players do
-        local thisleft = lim - Game.players[p].kills
+        local thisleft = lim - Game.players[p - 1].kills
         if (not leastleft) or thisleft < leastleft then
           leastleft = thisleft
         end
